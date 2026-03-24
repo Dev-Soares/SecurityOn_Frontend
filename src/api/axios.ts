91868541aa08cfb5674ff50ae879cda003c1eaac
Bernardo <bernardo.soares30@outlook.com>
2026-03-24 15:59:43 -0300

    test: changing variable to test the URL used

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:3333',
+    baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:2000',
     withCredentials: true, // Enviar cookies em requisições cross-origin
 });
